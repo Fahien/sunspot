889fa8524054991942b2b41f99d1efff9fed0733
Fahien <sir.fahien@gmail.com>
2016-10-19 16:13:10 +0200

    Set up the project

---
+++ compile-test.c
@@ -0,0 +1,11 @@
+#include <GL/glew.h> // Order matters
+#include <GLFW/glfw3.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	printf("Test succesfull\n");
+	return EXIT_SUCCESS;
+}
