8f6df29d220a44fe4a6baa8cbc6c51a20e5caffb
Fahien <sir.fahien@gmail.com>
2017-08-31 20:35:30 +0100

    Refactor Material

--- src/Material.cpp
+++ src/Material.cpp
@@ -2,53 +2,53 @@
 #include <sstream>
 
 #include "Material.h"
+#include "Logger.h"
 #include "Graphics.h"
 #include "ShaderProgram.h"
 
-using namespace sunspot;
+namespace sst = sunspot;
 
 
-const Logger Material::log{};
-
-
-Material::Material()
-	: Material("default")
-{}
-
-Material::Material(std::string &n)
-	: Material{ n.c_str() }
+sst::Material::Material()
+:	sst::Material("default")
 {}
 
 
-Material::Material(const char *n)
-	: name{ n }
-	, ambient {}
-	, diffuse {}
-	, specular{}
-	, hasAmbientMap { false }
-	, hasDiffuseMap { false }
-	, hasSpecularMap{ false }
+sst::Material::Material(std::string& n)
+:	sst::Material{ n.c_str() }
+{}
+
+
+sst::Material::Material(const char* n)
+:	name{ n }
+,	ambient {}
+,	diffuse {}
+,	specular{}
+,	hasAmbientMap { false }
+,	hasDiffuseMap { false }
+,	hasSpecularMap{ false }
 {
-	log.info("Material: created %s\n", n); // TODO remove debug log
+	sst::Logger::log.info("Material: created %s\n", n); // TODO remove debug log
 }
 
 
-Material::~Material()
+sst::Material::~Material()
 {
 	if (hasDiffuseMap)
 		glDeleteTextures(1, &diffuseMap);
 	if (hasSpecularMap)
 		glDeleteTextures(1, &specularMap);
 
-	log.info("Material: destroyed %s\n", name.c_str()); // TODO remove debug log
+	sst::Logger::log.info("Material: destroyed %s\n", name.c_str()); // TODO remove debug log
 }
 
 
-void Material::bind(const ShaderProgram *shader) const {
+void sst::Material::bind(const sst::ShaderProgram* shader) const
+{
 	// Bind ambient color
-	glUniform3f(shader->getLocation("material.ambient"), ambient.r, ambient.g, ambient.b);
+	glUniform3f(shader->getLocation("material.ambient"),  ambient.r,  ambient.g,  ambient.b);
 	// Bind diffuse color
-	glUniform3f(shader->getLocation("material.diffuse"), diffuse.r, diffuse.g, diffuse.b);
+	glUniform3f(shader->getLocation("material.diffuse"),  diffuse.r,  diffuse.g,  diffuse.b);
 	// Bind specular color
 	glUniform3f(shader->getLocation("material.specular"), specular.r, specular.g, specular.b);
 	// Bind shininess
@@ -57,7 +57,8 @@ void Material::bind(const ShaderProgram *shader) const {
 	// Bind diffuse flag
 	glUniform1i(shader->getLocation("material.hasDiffuseMap"), hasDiffuseMap);
 	// Bind diffuse map
-	if (hasDiffuseMap) {
+	if (hasDiffuseMap)
+	{
 		glUniform1i(shader->getLocation("material.diffuseMap"), 0);
 		glActiveTexture(GL_TEXTURE0);
 		glBindTexture(GL_TEXTURE_2D, diffuseMap);
@@ -65,7 +66,8 @@ void Material::bind(const ShaderProgram *shader) const {
 	// Bind specular flag
 	glUniform1i(shader->getLocation("material.hasSpecularMap"), hasSpecularMap);
 	// Bind specular map
-	if (hasSpecularMap) {
+	if (hasSpecularMap)
+	{
 		glUniform1i(shader->getLocation("material.specularMap"), 1);
 		glActiveTexture(GL_TEXTURE1);
 		glBindTexture(GL_TEXTURE_2D, specularMap);
@@ -73,9 +75,9 @@ void Material::bind(const ShaderProgram *shader) const {
 }
 
 
-std::ostream &sunspot::operator<<(std::ostream &os, const Material &m)
+std::ostream& sst::operator<<(std::ostream& os, const Material& m)
 {
-	return os << "Material[" << m.name << "]\n"
+	return os << "Material["      << m.name        << "]\n"
 	          << "\tambient["     << m.ambient     << "]\n"
 	          << "\tdiffuse["     << m.diffuse     << "]\n"
 	          << "\tspecular["    << m.specular    << "]\n"
--- src/Material.h
+++ src/Material.h
@@ -2,45 +2,46 @@
 #define SST_MATERIAL_H
 
 #include <fstream>
+
 #include "Graphics.h"
 #include "Color.h"
-#include "Logger.h"
 
 
-namespace sunspot {
-
+namespace sunspot
+{
 
 class ShaderProgram;
 
-
-struct Material {
-  public:
-	static const Logger log;
+struct Material
+{
+public:
 	Material();
-	Material(const char *n);
-	Material(std::string &n);
+	Material(const char* n);
+	Material(std::string& n);
 	~Material();
 
 	/// Bind the material to the shader
-	void bind(const ShaderProgram *shader) const;
+	void bind(const ShaderProgram* shader) const;
 
-	friend std::ostream &operator<<(std::ostream &os, const Material &mtl);
+	friend std::ostream& operator<<(std::ostream& os, const Material& mtl);
 
 	std::string name;
+
 	Color ambient;
 	Color diffuse;
 	Color specular;
 	float shininess = 32.0f;
+
 	bool hasAmbientMap;
 	GLuint ambientMap;
+
 	bool hasDiffuseMap;
 	GLuint diffuseMap;
+
 	bool hasSpecularMap;
 	GLuint specularMap;
 };
 
-
 }
-
 
 #endif // SST_MATERIAL_H
