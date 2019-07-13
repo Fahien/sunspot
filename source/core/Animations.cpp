5d3a773b1a29e920460ee09261b943e9b5c47b8c
Fahien <sir.fahien@gmail.com>
2019-07-13 17:17:42 +0100

    Improve animations
    - Rotate, scale, translate

--- source/core/Animations.cpp
+++ source/core/Animations.cpp
@@ -1,6 +1,9 @@
 #include "sunspot/core/Animations.h"
 #include "logspot/Log.h"
 
+#include <algorithm>
+
+
 namespace sunspot
 {
 void Animations::update( const float delta_time, gltfspot::Gltf& gltf ) const
@@ -9,78 +12,112 @@ void Animations::update( const float delta_time, gltfspot::Gltf& gltf ) const
 
 	for ( auto& animation : gltf.get_animations() )
 	{
+		animation.time.current += delta_time;
+
+		for ( auto& sampler : animation.samplers )
+		{
+			// get time for keyframes
+			auto&              time = gltf.GetAccessors().at( sampler.input );
+			std::vector<float> times( time.count );
+			auto&              view   = gltf.GetBufferViews().at( time.bufferView );
+			auto               offset = time.byteOffset + view.byteOffset;
+			auto&              buffer = gltf.GetBuffer( view.buffer );
+			std::memcpy( times.data(), &buffer[offset], time.count * sizeof( float ) );
+
+			auto it = std::max_element( std::begin( times ), std::end( times ) );
+			if ( it != std::end( times ) )
+			{
+				animation.time.max = std::max<float>( animation.time.max, *it );
+			}
+		}
+
+
+		// Start from the beginning
+		if ( animation.time.current >= animation.time.max )
+		{
+			// Reset time
+			animation.time.current -= animation.time.max;
+		}
+
 		for ( size_t i = 0; i < animation.channels.size(); ++i )
 		{
 			auto& channel = animation.channels[i];
-			channel.time += delta_time / 4.0f;
 
 			auto  node    = channel.target.node;
 			auto& sampler = animation.samplers.at( channel.sampler );
 			// get time for keyframes
-			auto& time   = gltf.GetAccessors().at( sampler.input );
-			std::vector<float> times(time.count);
-			auto& view = gltf.GetBufferViews().at( time.bufferView );
-			auto offset = time.byteOffset + view.byteOffset;
-			auto& buffer = gltf.GetBuffer( view.buffer );
-			std::memcpy(times.data(), &buffer[offset], time.count * sizeof(float));
+			auto&              time = gltf.GetAccessors().at( sampler.input );
+			std::vector<float> times( time.count );
+			auto&              view   = gltf.GetBufferViews().at( time.bufferView );
+			auto               offset = time.byteOffset + view.byteOffset;
+			auto&              buffer = gltf.GetBuffer( view.buffer );
+			std::memcpy( times.data(), &buffer[offset], time.count * sizeof( float ) );
 
 			// Find the keyframe
 			size_t keyframe = 1;
-			for (size_t i = 1; i < time.count; ++i)
+			for ( size_t i = 1; i < time.count; ++i )
 			{
-				if (channel.time > times[i])
+				if ( animation.time.current > times[i] )
 				{
 					keyframe++;
+				}
+				else
+				{
+					break;
 				}
 			}
 
 			// Start from the beginning
-			if (keyframe >= time.count)
+			if ( keyframe >= time.count )
 			{
 				keyframe = 1;
-				// Reset channel time
-				channel.time -= times[time.count - 1];
-				channel.time = 0.0f;
 			}
 
-			// Normalize time for lerp
-			float norm_time = (channel.time - times[keyframe-1]) / times[keyframe];
-			logspot::Log::info("channel.time: %f", channel.time);
-			logspot::Log::info("keyframe: %d", keyframe);
-
-			auto& values = gltf.GetAccessors().at( sampler.output );
-
-			switch ( channel.target.path )
+			if ( animation.time.current >= times[keyframe - 1] && animation.time.current <= times[keyframe] )
 			{
-				default:
-					assert( false && "Animation path not supported" );
-					break;
-				case Gltf::Animation::Target::Path::Rotation:
+				// Normalize time for lerp
+				float norm_time =
+				    ( animation.time.current - times[keyframe - 1] ) / ( times[keyframe] - times[keyframe - 1] );
+				logspot::Log::info( "norm[%f] chan[%f]", norm_time, animation.time.current );
+
+				auto& values = gltf.GetAccessors().at( sampler.output );
+
+				auto  view_index   = values.bufferView;
+				auto& data_view    = gltf.GetBufferViews().at( view_index );
+				auto  data_offset  = values.byteOffset + data_view.byteOffset;
+				auto  buffer_index = data_view.buffer;
+				auto& data_buffer  = gltf.GetBuffer( buffer_index );
+
+				switch ( channel.target.path )
 				{
-					auto  offset     = values.byteOffset;
-					auto  view_index = values.bufferView;
-					auto& view       = gltf.GetBufferViews().at( view_index );
-					offset += view.byteOffset;
-					auto           buffer_index = view.buffer;
-					auto&          buffer       = gltf.GetBuffer( buffer_index );
-					std::vector<mathspot::Quat> quats(values.count);
-					std::memcpy( quats.data(), &buffer[offset], values.count * sizeof(mathspot::Quat) );
+					default:
+						assert( false && "Animation path not supported" );
+						break;
+					case Gltf::Animation::Target::Path::Rotation:
+					{
+						std::vector<mathspot::Quat> quats( values.count );
+						std::memcpy( quats.data(), &data_buffer[data_offset], values.count * sizeof( mathspot::Quat ) );
+						node->rotation = mathspot::slerp( quats[keyframe - 1], quats[keyframe], norm_time );
+						break;
+					}
+					case Gltf::Animation::Target::Path::Scale:
+					{
+						std::vector<mathspot::Vec3> scales( values.count );
+						std::memcpy( scales.data(), &data_buffer[data_offset], values.count * sizeof( mathspot::Vec3 ) );
+						node->scale = mathspot::lerp( scales[keyframe - 1], scales[keyframe], norm_time );
+						break;
+					}
+					case Gltf::Animation::Target::Path::Translation:
+					{
+						std::vector<mathspot::Vec3> trans( values.count );
+						std::memcpy( trans.data(), &data_buffer[data_offset], values.count * sizeof( mathspot::Vec3 ) );
+						node->translation = mathspot::lerp( trans[keyframe - 1], trans[keyframe], norm_time );
 
-					node->rotation.x = (1.0f - norm_time) * quats[keyframe - 1].x + norm_time * quats[keyframe].x;
-					node->rotation.y = (1.0f - norm_time) * quats[keyframe - 1].y + norm_time * quats[keyframe].y;
-					node->rotation.z = (1.0f - norm_time) * quats[keyframe - 1].z + norm_time * quats[keyframe].z;
-					node->rotation.w = (1.0f - norm_time) * quats[keyframe - 1].w + norm_time * quats[keyframe].w;
-
-					break;
+						break;
+					}
+					case Gltf::Animation::Target::Path::Weights:
+						break;
 				}
-				case Gltf::Animation::Target::Path::Scale:
-
-					break;
-				case Gltf::Animation::Target::Path::Translation:
-
-					break;
-				case Gltf::Animation::Target::Path::Weights:
-					break;
 			}
 		}
 	}
